05ec5cbc8d5c16f45d7e982f9309c8aa0786d0e7
mburakergenc <muhammetergenc@gmail.com>
2017-11-13 22:46:10 +0300

    nesne_tanima_CNN_CIFAR10_RESNET.py açıklaması eklendi

--- KERAS/nesne_tanima_CNN_CIFAR10_RESNET.py
+++ KERAS/nesne_tanima_CNN_CIFAR10_RESNET.py
@@ -1,10 +1,12 @@
-"""Trains a ResNet on the CIFAR10 dataset.
 
-Greater than 91% test accuracy (0.52 val_loss) after 50 epochs
-48sec per epoch on GTX 1080Ti
+"""
+Amaç: Verilen görseldeki nesmeyi tanımak
+Yöntem: CIFAR10 veri seti üzerinde ResNet eğitmek
+Veriseti: CIFAR10 (https://www.cs.toronto.edu/~kriz/cifar.html)
+Algoritma: Evrişimli Sinir Ağları (Convolutional Neural Networks)
 
-Deep Residual Learning for Image Recognition
-https://arxiv.org/pdf/1512.03385.pdf
+50 epoch sonunda 91% üzerinde test doğruluk oranı elde ediliyor.
+GTX 1080Ti kullanarak her bir epoch 48 saniye sürüyor.
 """
 
 from __future__ import print_function
@@ -21,23 +23,23 @@ from keras.datasets import cifar10
 import numpy as np
 import os
 
-# Training params.
+# Parametrelerin eğitilmesi
 batch_size = 32
 epochs = 100
 data_augmentation = True
 
-# Network architecture params.
+# Ağ mimarisi parametreleri
 num_classes = 10
 num_filters = 64
 num_blocks = 4
 num_sub_blocks = 2
 use_max_pool = False
 
-# Load the CIFAR10 data.
+# Veri setinin yüklenip eğitim/test olarak ayrılması
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
-# Input image dimensions.
-# We assume data format "channels_last".
+# Giriş resimlerinin boyutları
+# Varsayılan veri formatı "channels_last".
 img_rows = x_train.shape[1]
 img_cols = x_train.shape[2]
 channels = x_train.shape[3]
@@ -57,7 +59,7 @@ else:
     x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, channels)
     input_shape = (img_rows, img_cols, channels)
 
-# Normalize data.
+# Verinin kesirli hale getirilip normalleştirilmesi
 x_train = x_train.astype('float32') / 255
 x_test = x_test.astype('float32') / 255
 print('x_train shape:', x_train.shape)
@@ -65,7 +67,7 @@ print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 print('y_train shape:', y_train.shape)
 
-# Convert class vectors to binary class matrices.
+# Sınıf vektörlerinin ikili sınıf matrislerine dönüştürülmesi
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
@@ -80,14 +82,14 @@ x = Conv2D(num_filters,
 x = BatchNormalization()(x)
 x = Activation('relu')(x)
 
-# Orig paper uses max pool after 1st conv.
-# Reaches up 87% acc if use_max_pool = True.
-# Cifar10 images are already too small at 32x32 to be maxpooled. So, we skip.
+# Orjinal makale 1. evrişimden sonra max pool metodunu kullanmaktadır.
+# use_max_pool = True olduğunda doğruluk oranı 87%'ye kadar çıkıyor.
+# Cifar10 veri setinin resimleri max pool metodunu kullanmak için çok küçük(32x32). Bu nedenle bu yöntemi atlıyoruz.
 if use_max_pool:
     x = MaxPooling2D(pool_size=3, strides=2, padding='same')(x)
     num_blocks = 3
 
-# Instantiate convolutional base (stack of blocks).
+# Modelin temelinin oluşturulması
 for i in range(num_blocks):
     for j in range(num_sub_blocks):
         strides = 1
@@ -120,21 +122,21 @@ for i in range(num_blocks):
 
     num_filters = 2 * num_filters
 
-# Add classifier on top.
+# Sınıflandırıcının en başa eklenmesi
 x = AveragePooling2D()(x)
 y = Flatten()(x)
 outputs = Dense(num_classes,
                 activation='softmax',
                 kernel_initializer='he_normal')(y)
 
-# Instantiate and compile model.
+# Modelin oluşturulup derlenmesi
 model = Model(inputs=inputs, outputs=outputs)
 model.compile(loss='categorical_crossentropy',
               optimizer=Adam(),
               metrics=['accuracy'])
 model.summary()
 
-# Prepare model model saving directory.
+# Model dizinin hazırlanıp, kaydedilmesi
 save_dir = os.path.join(os.getcwd(), 'saved_models')
 model_name = 'cifar10_resnet_model.h5'
 if not os.path.isdir(save_dir):
@@ -142,6 +144,7 @@ if not os.path.isdir(save_dir):
 filepath = os.path.join(save_dir, model_name)
 
 # Prepare callbacks for model saving and for learning rate decaying.
+# Modelin kaydedilmesi ve öğrenme değerinin azaltılması için geriçağırımların hazırlanması
 checkpoint = ModelCheckpoint(filepath=filepath,
                              verbose=1,
                              save_best_only=True)
@@ -151,7 +154,7 @@ lr_reducer = ReduceLROnPlateau(factor=np.sqrt(0.1),
                                min_lr=0.5e-6)
 callbacks = [checkpoint, lr_reducer]
 
-# Run training, with or without data augmentation.
+# Eğitimin başlatılması. Veri arttırma ile ya da veri arttırma olmaksızın.
 if not data_augmentation:
     print('Not using data augmentation.')
     model.fit(x_train, y_train,
@@ -162,21 +165,21 @@ if not data_augmentation:
               callbacks=callbacks)
 else:
     print('Using real-time data augmentation.')
-    # This will do preprocessing and realtime data augmentation:
+    # Ön işleme ve gerçek zamanlı veri arttırımının uygulanması
     datagen = ImageDataGenerator(
-        featurewise_center=False,  # set input mean to 0 over the dataset
-        samplewise_center=False,  # set each sample mean to 0
-        featurewise_std_normalization=False,  # divide inputs by std of the dataset
-        samplewise_std_normalization=False,  # divide each input by its std
-        zca_whitening=False,  # apply ZCA whitening
-        rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
-        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
-        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
-        horizontal_flip=True,  # randomly flip images
-        vertical_flip=False)  # randomly flip images
+        featurewise_center=False,  # Giriş verilerinin prtalamasının 0'lanması
+        samplewise_center=False,  # Herbir örnek verinin ortalamasının 0'a eşitlenmesi
+        featurewise_std_normalization=False, # Giriş verilerinin, veri setinin standart varyans değerine bölünmesi
+        samplewise_std_normalization=False,  # Herbir verinin standart varyans değerine bölünmesi
+        zca_whitening=False,  # "ZCA whitening" metodunun uygulanması
+        rotation_range=0,  # Resimlerin bir sınır aralığında gelişi güzel döndürülmesi (degrees, 0 to 180)
+        width_shift_range=0.1,  # Resimlerin gelişigüzel bir şekilde yatay olarak kaydırılması (toplam genişliğin bölümü)
+        height_shift_range=0.1,  # Resimlerin gelişigüzel bir şekilde dikey olarak kaydırılması(toplam yüksekliğin bölümü)
+        horizontal_flip=True,  # Resimlerin gelişigüzel bir şekilde yatay olarak çevirilmesi
+        vertical_flip=False)  # Resimlerin gelişigüzel bir şekilde dikey olarak çevirilmesi
 
-    # Compute quantities required for featurewise normalization
-    # (std, mean, and principal components if ZCA whitening is applied).
+    # Normalizasyon gerekliliklerinin hesaplanması
+    # (standart varyans, ortalama, ve asıl bileşenler eğer "ZCA whitening" uygulanıyorsa).
     datagen.fit(x_train)
 
     # Fit the model on the batches generated by datagen.flow().
@@ -186,7 +189,7 @@ else:
                         epochs=epochs, verbose=1, workers=4,
                         callbacks=callbacks)
 
-# Score trained model.
+# Eğitilmiş modelin başarısının ölçülmesi
 scores = model.evaluate(x_test, y_test, verbose=1)
 print('Test loss:', scores[0])
 print('Test accuracy:', scores[1])
